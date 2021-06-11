8aac506b40be266446a3862b8e0ce8dfa10347fb
chuan.jiang@2kchina.com <chuan.jiang@2kchina.com>
2021-06-11 15:22:42 +0800

    add

--- 086f/src/utils/fn.js
+++ 086f/src/utils/fn.js
@@ -3,18 +3,7 @@ import { message } from 'antd';
 import cookie from './cookie';
 
 export const makeHttpQuery = async (path, option) => {
-  // hostname
-<<<<<<< HEAD
-  // if (!cookie.get('token') && window.location.pathname !== '/login') {
-  //   window.location.replace('/login');
-  //   return;
-  // }
-=======
-  if (!cookie.get('token') && window.location.pathname !== '/login') {
-    window.location.replace('/login');
-    return;
-  }
->>>>>>> 9d37cb2a9513e12b913ebab3243d7cb91c70854f
+
   let hostname = `http://${window.location.hostname}:5001`;
   // url
   const url = hostname + path;
@@ -24,10 +13,6 @@ export const makeHttpQuery = async (path, option) => {
       Authorization: `${cookie.get('token')}`,
     },
   });
-<<<<<<< HEAD
-  console.log(res)
-=======
->>>>>>> 9d37cb2a9513e12b913ebab3243d7cb91c70854f
   if (res.data.error) {
     message.error(`${res.data.error.code}: ${res.data.error.message}`, 6);
     // return;
